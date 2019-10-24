0640faa7d319f23b984575fab564523209b2a63d
Josh Green <joshx3g@gmail.com>
2019-10-24 13:46:46 -0500

    Updates

---
+++ precache-manifest.c14859cc977ac647adc778a7a4d1d66b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01ddd18a14f588258ccf14fc400a0120",
+    "url": "/video-game-manager/index.html"
+  },
+  {
+    "revision": "8908186427692b4d80ac",
+    "url": "/video-game-manager/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "c4f135d9c9de99718c31",
+    "url": "/video-game-manager/static/js/2.874e19fc.chunk.js"
+  },
+  {
+    "revision": "8908186427692b4d80ac",
+    "url": "/video-game-manager/static/js/main.795da985.chunk.js"
+  },
+  {
+    "revision": "2ab0e230baa12502f7fa",
+    "url": "/video-game-manager/static/js/runtime-main.39d72c07.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/video-game-manager/static/media/logo.25bf045c.svg"
+  }
+]);
