6f7e8eef599f32af532eb141acc79c8c33997619
Josh Green <joshx3g@gmail.com>
2019-10-25 11:36:52 -0500

    Playing around with styling and material ui

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,48 @@
 import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import './Home.css';
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: '', items: [] };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
   render() {
     return (
-      <form>
-        <label>
-          Name:
-          <input
-            type='text'
-            name='gamename'
-            placeholder='Please type the name of a video game'
+      <div className='container'>
+        <form onSubmit={this.handleSubmit}>
+          <TextField
+            className='input'
+            id='outlined-name'
+            label='Name'
+            value={this.state.value}
+            onChange={this.handleChange}
+            margin='normal'
+            variant='outlined'
           />
-          <input type='submit' value='Submit' />
-        </label>
-      </form>
+          <label>
+            <br />
+            <br />
+            <Button variant='contained' color='default' type='submit'>
+              Test
+            </Button>
+          </label>
+        </form>
+      </div>
     );
   }
 }
