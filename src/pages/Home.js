53400348862c56a6a3234df9389d7f1863dd8972
Josh Green <joshx3g@gmail.com>
2019-10-28 07:54:05 -0500

    added Materialize to the project, messing around with cards and displaying game info

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -53,9 +53,23 @@ class Home extends Component {
           />
           {this.state.games.map(game => (
             <ul key={game.id}>
-              <li>
-                {game.name} {game.summary}
-              </li>
+              <div class='col s12 m7'>
+                <h2 class='header'> {game.name} </h2>
+                <div class='card horizontal'>
+                  <div class='card-image'>
+                    <img src='https://lorempixel.com/100/190/nature/6' />
+                  </div>
+                  <div class='card-stacked'>
+                    <div class='card-content'>
+                      <p>{game.summary}</p>
+                    </div>
+                    <div class='card-action'>
+                      <a href='#'>This is a link</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/* {game.name} {game.summary} */}
             </ul>
           ))}
         </form>
