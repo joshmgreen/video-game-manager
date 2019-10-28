36561a910fd85e6782f41b78165d9ee9433c74f3
Josh Green <joshx3g@gmail.com>
2019-10-28 07:57:40 -0500

    changed layout and styling, WIP

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,7 @@ class Home extends Component {
     this.state = {
       games: [],
       isLoaded: false,
-      query: 'Zelda'
+      query: ''
     };
   }
 
@@ -57,15 +57,13 @@ class Home extends Component {
                 <h2 class='header'> {game.name} </h2>
                 <div class='card horizontal'>
                   <div class='card-image'>
-                    <img src='https://lorempixel.com/100/190/nature/6' />
+                    <img alt='' src='https://lorempixel.com/100/190/nature/6' />
                   </div>
                   <div class='card-stacked'>
                     <div class='card-content'>
                       <p>{game.summary}</p>
                     </div>
-                    <div class='card-action'>
-                      <a href='#'>This is a link</a>
-                    </div>
+                    <div class='card-action'></div>
                   </div>
                 </div>
               </div>
